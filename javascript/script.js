0dc86f9343c605d5b2ecee49f7e9f59d16158ded
iaio998 <depalatisiacopo@gmail.com>
2023-10-16 16:45:45 +0200

    Workign exercise without for/while

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,18 @@ const btn = document.querySelector("button");
 const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 let msg;
+
+const list = [];
+
+//// SOLUZIONE SENZA CICLI
+// btn.addEventListener("click", function () {
+//   const listElements = inputEl.value;
+//   list.push(listElements);
+//   console.log(list);
+//   if (listElements === "Stop".toLocaleLowerCase()) {
+//     list.pop();
+//     msg = `Gentile utente ecco a lei la lista del carrello: ${list}`;
+//     resultEl.classList.remove("d-none");
+//     resultEl.innerHTML = msg;
+//   }
+// });
